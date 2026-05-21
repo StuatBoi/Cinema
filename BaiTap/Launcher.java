04ce8c296c3b3be3033197a1ff961d850cca68fe
StuatBoi <hoanganhle121106@gmail.com>
2026-05-21 21:07:43 +0700

    Delete BaiTap directory

--- BaiTap/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.net.demo;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(MainApp.class, args);
-    }
-}
